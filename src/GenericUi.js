cc5e63691882a3d312dc4bcdecaa0eea58505914
Kumar <kumarelubandi@gmail.com>
2020-03-01 00:17:52 +0530

    ui type session store

--- src/GenericUi.js
+++ src/GenericUi.js
@@ -6,6 +6,16 @@ import ChoiceInput from './components/Inputs/ChoiceInput/ChoiceInput';
 import DocumentInput from './components/Inputs/DocumentInput/DocumentInput';
 import Loader from 'react-loader-spinner';
 import BooleanInput from './components/Inputs/BooleanInput/BooleanInput';
+import DropdownServiceCode from './components/DropdownServiceCode';
+
+import DropdownCoverage from './components/DropdownCoverage';
+
+import { DateInput } from 'semantic-ui-calendar-react';
+
+import { Dropdown } from 'semantic-ui-react';
+
+import DropdownEncounter from './components/DropdownEncounter';
+import SelectPayer from './components/SelectPayer';
 
 
 export default class GenericUi {
@@ -19,6 +29,216 @@ export default class GenericUi {
               <Testing logs={logs} />
             </div>
          );
+    }
+    getProviderRequestUI(inputThis){
+        return (
+      <React.Fragment>
+        <div>
+          <header id="inpageheader">
+            <div className="container">
+
+              <div id="logo" className="pull-left">
+                <h1><a href="#intro" className="scrollto">Beryllium</a></h1>
+                {/* <a href="#intro"><img src={process.env.PUBLIC_URL + "/assets/img/logo.png"} alt="" title="" /></a> */}
+              </div>
+
+              <nav id="nav-menu-container">
+                <ul className="nav-menu">
+                  {/* <li className="menu-has-children"><a href="">{localStorage.getItem('username')}</a>
+                    <ul>
+                      <li><a href="" onClick={inputThis.onClickLogout}>Logout</a></li>
+                    </ul>
+                  </li> */}
+                </ul>
+              </nav>
+            </div>
+          </header>
+          <div id="main" style={{ marginTop: "92px" }}>
+
+            <div className="form">
+              <div className="container">
+                <div className="section-header">
+                  <h3>Prior Authorization</h3>
+                  <p>Submit your request to check for prior authorization.</p>
+                </div>
+                <div className="form-row">
+                  <div className="form-group col-md-3 offset-1">
+                    <h4 className="title">NPI</h4>
+                  </div>
+                  <div className="form-group col-md-8">
+                    <input type="text" name="practitioner" className="form-control" id="name" placeholder="Practitioner NPI"
+                      value={inputThis.state.practitionerId} onChange={inputThis.onPractitionerChange}
+                      data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                    <div className="validation"></div>
+                  </div>
+                </div>
+                {(inputThis.state.hookName === 'order-review' || inputThis.state.hookName === 'order-select') &&
+                  <div>
+
+                    <div className="form-row">
+                      <div className="form-group col-md-3 offset-1">
+                        <h4 className="title">Beneficiary Id</h4>
+                      </div>
+                      <div className="form-group col-md-4">
+                        <input type="text" name="patient" className="form-control" id="name" placeholder="Beneficiary Id"
+                          value={inputThis.state.patientId} onChange={inputThis.onPatientChange}
+                          data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <div className="validation"></div>
+                      </div>
+                      <div className="form-group col-md-4">
+                        <button type="button" onClick={inputThis.handlePrefetch}>Prefetch
+                        <div id="fse" className={"spinner " + (inputThis.state.prefetchloading ? "visible" : "invisible")}>
+                            <Loader
+                              type="Oval"
+                              color="#fff"
+                              height={15}
+                              width={15}
+                            />
+                          </div>
+                        </button>
+                      </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="form-group col-md-3 offset-1">
+                        <h4 className="title">Patient Info*</h4>
+                      </div>
+                      <div className="form-group col-md-4">
+                        <input type="text" name="firstName" className="form-control" id="name" placeholder="First Name"
+                          value={inputThis.state.firstName} onChange={inputThis.onChangeFirstName}
+                          data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <div className="validation"></div>
+                      </div>
+                      <div className="form-group col-md-4">
+                        <input type="text" name="lastName" className="form-control" id="name" placeholder="Last Name"
+                          value={inputThis.state.lastName} onChange={inputThis.onChangeLastName}
+                          data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <div className="validation"></div>
+                      </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="form-group col-md-3 offset-1">
+                        <h4 className="title"></h4>
+                      </div>
+                      <div className="form-group col-md-4">
+                        <Dropdown
+                          className={"blackBorder"}
+                          options={inputThis.state.genderOptions}
+                          placeholder='Gender'
+                          search
+                          selection
+                          fluid
+                          value={inputThis.state.gender}
+                          onChange={inputThis.handleGenderChange}
+                        />
+                      </div>
+                      <div className="form-group col-md-4">
+                        <DateInput
+                          name="birthDate"
+                          placeholder="Birth Date"
+                          dateFormat="MM/DD/YYYY"
+                          fluid
+                          value={inputThis.state.birthDate}
+                          iconPosition="left"
+                          onChange={inputThis.changebirthDate}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-row">
+                      <div className="form-group col-md-3 offset-1">
+                        <h4 className="title"></h4>
+                      </div>
+                      <div className="form-group col-md-4">
+                        <Dropdown
+                          className={"blackBorder"}
+                          options={inputThis.state.stateOptions}
+                          placeholder='State'
+                          search
+                          selection
+                          fluid
+                          value={inputThis.state.patientState}
+                          onChange={inputThis.handlePatientStateChange}
+                        />
+                      </div>
+                      <div className="form-group col-md-4">
+                        <input type="text" name="patientPostalCoade" className="form-control" id="name" placeholder="Postal Code"
+                          value={inputThis.state.patientPostalCode} onChange={inputThis.onPatientPostalChange}
+                          data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <div className="validation"></div>
+                      </div>
+                    </div>
+
+                    {/* {inputThis.state.validatePatient === true &&
+                      <div className='errorMsg dropdown'>{inputThis.props.config.errorMsg}</div>
+                    } */}
+                  </div>}
+                {inputThis.state.encounters.length > 0 &&
+                  <div className="form-row">
+                    <div className="form-group col-md-3 offset-1">
+                      <h4 className="title">Encounter</h4>
+                    </div>
+                    <div className="form-group col-md-8">
+                      <DropdownEncounter elementName="encounterId" encounters={inputThis.state.encounters} updateCB={inputThis.updateStateElement} />
+                    </div>
+                  </div>
+                }
+                {inputThis.state.coverageResources.length > 0 &&
+                  <div className="form-row">
+                    <div className="form-group col-md-3 offset-1">
+                      <h4 className="title">Coverage</h4>
+                    </div>
+                    <div className="form-group col-md-8">
+                      <DropdownCoverage elementName="coverageId" coverages={inputThis.state.coverageResources} updateCB={inputThis.updateStateElement} />
+                      {/*inputThis.state.coverageResources.map((element) => {
+                        return (
+                          <div key={element.id}>
+                            <button
+                              className={"radio-button btn " + (inputThis.state.coverageId === element.id ? "selected" : null)}
+                              onClick={() => {
+                                inputThis.setCoverage(element)
+                              }}
+                            >
+                            </button>
+                            <span className="text-radio tooltip-x">
+                              <div><b>Coverage</b></div>
+                              {// <div><b>Status</b>: {element.status}</div> }
+                              {// <div><b>Subscriber</b>: {element.subscriber.display}</div> }
+                              <div><b>Beneficiary</b>: {element.beneficiary.display}</div>
+                              {// <div><b>Coverage Start Date</b>: {dateFormat(element.period.start,"fulldate")}</div> }
+                              <div><b>Payor</b>: {element.payor[0].display}</div>
+                              {// <div><b>Class</b>: plan: Value: {element.class[0].value} Name: {element.class[0].name}</div> }
+                            </span>
+                          </div>
+                        )
+                      }) 
+                    */}
+                    </div>
+                  </div>}
+                {inputThis.state.coverageResources.length === 0 &&
+                  <SelectPayer elementName='payer' updateCB={inputThis.updateStateElement} />
+                }
+                <DropdownServiceCode elementName="selected_codes" updateCB={inputThis.updateStateElement} />
+                <div className="text-center">
+                  <button type="button" onClick={inputThis.startLoading}>Submit
+                    <div id="fse" className={"spinner " + (inputThis.state.loading ? "visible" : "invisible")}>
+                      <Loader
+                        type="Oval"
+                        color="#fff"
+                        height={15}
+                        width={15}
+                      />
+                    </div>
+                  </button>
+                </div>
+                <div>
+                  {inputThis.state.crd_error_msg &&
+                    <div className="text-center"><p>{inputThis.state.crd_error_msg}</p></div>
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment >)
     }
     getQuestionnaireFormApp(smart,questionnaire,cqlPrepoulationResults,serviceRequest,bundle,claimEndpoint) {
         return (
--- src/ProviderRequest.js
+++ src/ProviderRequest.js
@@ -31,6 +31,7 @@ const types = {
 class ProviderRequest extends Component {
   constructor(props) {
     super(props);
+    this.ui = new UiFactory().getUi();
     this.state = {
       config: sessionStorage.getItem('config') !== undefined ? JSON.parse(sessionStorage.getItem('config')) : {},
       patient: null,
@@ -104,7 +105,7 @@ class ProviderRequest extends Component {
       status: (foo => { return foo !== "draft" && foo !== "open" }),
       code: (foo => { return !foo.match(/^[a-z0-9]+$/i) })
     };
-
+    
     this.medication_prescribe = false;
     this.startLoading = this.startLoading.bind(this);
     this.submit_info = this.submit_info.bind(this);
@@ -448,214 +449,8 @@ class ProviderRequest extends Component {
     }
   }
   renderClaimSubmit() {
-    return (
-      <React.Fragment>
-        <div>
-          <header id="inpageheader">
-            <div className="container">
-
-              <div id="logo" className="pull-left">
-                <h1><a href="#intro" className="scrollto">Beryllium</a></h1>
-                {/* <a href="#intro"><img src={process.env.PUBLIC_URL + "/assets/img/logo.png"} alt="" title="" /></a> */}
-              </div>
-
-              <nav id="nav-menu-container">
-                <ul className="nav-menu">
-                  {/* <li className="menu-has-children"><a href="">{localStorage.getItem('username')}</a>
-                    <ul>
-                      <li><a href="" onClick={this.onClickLogout}>Logout</a></li>
-                    </ul>
-                  </li> */}
-                </ul>
-              </nav>
-            </div>
-          </header>
-          <div id="main" style={{ marginTop: "92px" }}>
-
-            <div className="form">
-              <div className="container">
-                <div className="section-header">
-                  <h3>Prior Authorization</h3>
-                  <p>Submit your request to check for prior authorization.</p>
-                </div>
-                <div className="form-row">
-                  <div className="form-group col-md-3 offset-1">
-                    <h4 className="title">NPI</h4>
-                  </div>
-                  <div className="form-group col-md-8">
-                    <input type="text" name="practitioner" className="form-control" id="name" placeholder="Practitioner NPI"
-                      value={this.state.practitionerId} onChange={this.onPractitionerChange}
-                      data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                    <div className="validation"></div>
-                  </div>
-                </div>
-                {(this.state.hookName === 'order-review' || this.state.hookName === 'order-select') &&
-                  <div>
-
-                    <div className="form-row">
-                      <div className="form-group col-md-3 offset-1">
-                        <h4 className="title">Beneficiary Id</h4>
-                      </div>
-                      <div className="form-group col-md-4">
-                        <input type="text" name="patient" className="form-control" id="name" placeholder="Beneficiary Id"
-                          value={this.state.patientId} onChange={this.onPatientChange}
-                          data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                        <div className="validation"></div>
-                      </div>
-                      <div className="form-group col-md-4">
-                        <button type="button" onClick={this.handlePrefetch}>Prefetch
-                        <div id="fse" className={"spinner " + (this.state.prefetchloading ? "visible" : "invisible")}>
-                            <Loader
-                              type="Oval"
-                              color="#fff"
-                              height={15}
-                              width={15}
-                            />
-                          </div>
-                        </button>
-                      </div>
-                    </div>
-                    <div className="form-row">
-                      <div className="form-group col-md-3 offset-1">
-                        <h4 className="title">Patient Info*</h4>
-                      </div>
-                      <div className="form-group col-md-4">
-                        <input type="text" name="firstName" className="form-control" id="name" placeholder="First Name"
-                          value={this.state.firstName} onChange={this.onChangeFirstName}
-                          data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                        <div className="validation"></div>
-                      </div>
-                      <div className="form-group col-md-4">
-                        <input type="text" name="lastName" className="form-control" id="name" placeholder="Last Name"
-                          value={this.state.lastName} onChange={this.onChangeLastName}
-                          data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                        <div className="validation"></div>
-                      </div>
-                    </div>
-                    <div className="form-row">
-                      <div className="form-group col-md-3 offset-1">
-                        <h4 className="title"></h4>
-                      </div>
-                      <div className="form-group col-md-4">
-                        <Dropdown
-                          className={"blackBorder"}
-                          options={this.state.genderOptions}
-                          placeholder='Gender'
-                          search
-                          selection
-                          fluid
-                          value={this.state.gender}
-                          onChange={this.handleGenderChange}
-                        />
-                      </div>
-                      <div className="form-group col-md-4">
-                        <DateInput
-                          name="birthDate"
-                          placeholder="Birth Date"
-                          dateFormat="MM/DD/YYYY"
-                          fluid
-                          value={this.state.birthDate}
-                          iconPosition="left"
-                          onChange={this.changebirthDate}
-                        />
-                      </div>
-                    </div>
-                    <div className="form-row">
-                      <div className="form-group col-md-3 offset-1">
-                        <h4 className="title"></h4>
-                      </div>
-                      <div className="form-group col-md-4">
-                        <Dropdown
-                          className={"blackBorder"}
-                          options={this.state.stateOptions}
-                          placeholder='State'
-                          search
-                          selection
-                          fluid
-                          value={this.state.patientState}
-                          onChange={this.handlePatientStateChange}
-                        />
-                      </div>
-                      <div className="form-group col-md-4">
-                        <input type="text" name="patientPostalCoade" className="form-control" id="name" placeholder="Postal Code"
-                          value={this.state.patientPostalCode} onChange={this.onPatientPostalChange}
-                          data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                        <div className="validation"></div>
-                      </div>
-                    </div>
-
-                    {/* {this.state.validatePatient === true &&
-                      <div className='errorMsg dropdown'>{this.props.config.errorMsg}</div>
-                    } */}
-                  </div>}
-                {this.state.encounters.length > 0 &&
-                  <div className="form-row">
-                    <div className="form-group col-md-3 offset-1">
-                      <h4 className="title">Encounter</h4>
-                    </div>
-                    <div className="form-group col-md-8">
-                      <DropdownEncounter elementName="encounterId" encounters={this.state.encounters} updateCB={this.updateStateElement} />
-                    </div>
-                  </div>
-                }
-                {this.state.coverageResources.length > 0 &&
-                  <div className="form-row">
-                    <div className="form-group col-md-3 offset-1">
-                      <h4 className="title">Coverage</h4>
-                    </div>
-                    <div className="form-group col-md-8">
-                      <DropdownCoverage elementName="coverageId" coverages={this.state.coverageResources} updateCB={this.updateStateElement} />
-                      {/*this.state.coverageResources.map((element) => {
-                        return (
-                          <div key={element.id}>
-                            <button
-                              className={"radio-button btn " + (this.state.coverageId === element.id ? "selected" : null)}
-                              onClick={() => {
-                                this.setCoverage(element)
-                              }}
-                            >
-                            </button>
-                            <span className="text-radio tooltip-x">
-                              <div><b>Coverage</b></div>
-                              {// <div><b>Status</b>: {element.status}</div> }
-                              {// <div><b>Subscriber</b>: {element.subscriber.display}</div> }
-                              <div><b>Beneficiary</b>: {element.beneficiary.display}</div>
-                              {// <div><b>Coverage Start Date</b>: {dateFormat(element.period.start,"fulldate")}</div> }
-                              <div><b>Payor</b>: {element.payor[0].display}</div>
-                              {// <div><b>Class</b>: plan: Value: {element.class[0].value} Name: {element.class[0].name}</div> }
-                            </span>
-                          </div>
-                        )
-                      }) 
-                    */}
-                    </div>
-                  </div>}
-                {this.state.coverageResources.length === 0 &&
-                  <SelectPayer elementName='payer' updateCB={this.updateStateElement} />
-                }
-                <DropdownServiceCode elementName="selected_codes" updateCB={this.updateStateElement} />
-                <div className="text-center">
-                  <button type="button" onClick={this.startLoading}>Submit
-                    <div id="fse" className={"spinner " + (this.state.loading ? "visible" : "invisible")}>
-                      <Loader
-                        type="Oval"
-                        color="#fff"
-                        height={15}
-                        width={15}
-                      />
-                    </div>
-                  </button>
-                </div>
-                <div>
-                  {this.state.crd_error_msg &&
-                    <div className="text-center"><p>{this.state.crd_error_msg}</p></div>
-                  }
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </React.Fragment >);
+    console.log(this.ui);
+    return this.ui.getProviderRequestUI(this);
   };
 
   async getRequestID(token) {
